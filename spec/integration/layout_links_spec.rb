64a9b0526fedaf3cde2e44c22add9328abf6a842
Doug Ito <basemile@gmail.com>
2010-03-20 07:40:53 -0700

    added webrat tests to check links

--- spec/integration/layout_links_spec.rb
+++ spec/integration/layout_links_spec.rb
@@ -1,10 +1,7 @@
 require 'spec_helper'
+require 'test_helper'
 
 describe "LayoutLinks" do
-  
-  require 'spec_helper'
-
-  describe "Layout links" do
 
     it "should have a Home page at '/'" do
       get '/'
@@ -31,6 +28,19 @@ describe "LayoutLinks" do
       response.should render_template('users/new')
     end
     
-  end
+    it "should have the right links on the layout" do
+      visit root_path
+      click_link "About"
+      response.should render_template('pages/about')
+      click_link "Help"
+      response.should render_template('pages/help')
+      click_link "Contact"
+      response.should render_template('pages/contact')
+      click_link "Home"
+      response.should render_template('pages/home')
+      click_link "Sign up now!"
+      response.should render_template('users/new')
+    end
+
   
 end
