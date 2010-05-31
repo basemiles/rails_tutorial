66d90a53a31cead88dd050e4ae5e84e0f8d8d18e
Doug Ito <basemile@gmail.com>
2010-05-31 07:19:28 -0700

    Added flash, signin

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -22,7 +22,7 @@ describe UsersController do
     before(:each) do
       @user = Factory(:user)
       # Arrange for User.find (params[:id] to find the right user.)
-      User.stub! (:find, @user.id).and_return(@user)
+      User.stub! (:find,@user.id).and_return(@user)
     end
     
     it "should be successful" do
@@ -32,7 +32,7 @@ describe UsersController do
     
     it "should have the right title" do
       get :show, :id => @user
-      response.should have_tag("title", /#{user.name}/)
+      response.should have_tag("title", /#{@user.name}/)
     end
     
     it "should have the user's name" do
@@ -40,12 +40,55 @@ describe UsersController do
       response.should have_tag("h2", /#{@user.name}/)
     end
    
-  # 5/13 Uncomment when gravatar plugin is installed 
-  #  it "should have a profile image" do
-  #    get :show, id => @user
-   #   response.should have_tag("h2>img", :class => "gravatar")
-  #  end
+
+   it "should have a profile image" do
+     get :show, id => @user
+     response.should have_tag("h2>img", :class => "gravatar")
+   end
   end
   
-  
+  describe "POST 'create" do
+    
+    describe "failure" do
+      before(:each) do
+        @attr = { :name => "", :email => "", :password => "",
+          :password_confirmation => ""}
+        @user = Factory.build(:user, @attr)
+        User.stub!(:new).and_return(@user)
+        @user.should_receive(:save).and_return(false)
+      end
+      
+      it "should have the right title" do
+        post :create, :user => @attr
+        response.should have_tag("title", /sign up/i)
+      end
+      
+      it "should render the 'new' page" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
+    end
+    
+    describe "success" do
+      
+      before(:each) do
+        @attr = { :name => "New User", :email => "user@example.com",
+                  :password => "foobar", :password_confirmation => "foobar"}
+        @user = Factory(:user, @attr)
+        User.stub!(:new).and_return(@user)
+        @user.should_receive(:save).and_return(true)
+      end
+      
+      it "should redirect to the user show page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(@user))
+      end
+      
+      it "should have a welcome message" do
+        post :create, :user => @attr
+        flash[:success].should =~ /welcome to the sample app/i
+      end
+    end
+        
+  end
 end
