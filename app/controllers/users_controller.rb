a3588645eed1ce2a43998a2cf10c562649b7fedf
Doug Ito <basemile@gmail.com>
2010-06-24 07:07:26 -0700

    loaded fake users, finished pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:index, :edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
+  
   def new
     @title = "Sign up"
     @user = User.new
@@ -22,4 +25,33 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @title = "Edit user"
+  end
+  
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+  
+  def index
+    @title = "All users"
+    @users = User.paginate(:page => params[:page])
+  end
+  
+  private
+  
+    def authenticate
+      deny_access unless signed_in?
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end  
 end
